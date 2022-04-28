8f2172a4909f546b6afe3e26e1fb24736825c3a5
docong1002 <97docong@gmail.com>
2022-04-29 01:02:04 +0900

    초기 설정

---
+++ src/main/java/com/team1/stelling/StellingApplication.java
@@ -0,0 +1,13 @@
+package com.team1.stelling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StellingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StellingApplication.class, args);
+    }
+
+}
