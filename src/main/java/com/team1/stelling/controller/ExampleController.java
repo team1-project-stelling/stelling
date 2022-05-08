c294da46d3a67c6ef8769de1f96ac754015214da
docong1002 <96901629+docong1002@users.noreply.github.com>
2022-05-09 05:55:53 +0900

    재희 코인샵+내서재 수정

--- src/main/java/com/team1/stelling/controller/ExampleController.java
+++ src/main/java/com/team1/stelling/controller/ExampleController.java
@@ -2,6 +2,7 @@ package com.team1.stelling.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,5 +13,23 @@ public class ExampleController {
     @RequestMapping("")
     public void myLibrary(){
         log.info("libaray");
+    }
+
+    @GetMapping("/myBook")
+    public String myBook(){
+        log.info("myBook");
+        return "myLibaray/myBook";
+    }
+
+    @GetMapping("/myRead")
+    public String myRead(){
+        log.info("myRead");
+        return "myLibaray/myRead";
+    }
+
+    @GetMapping("/coinShop")
+    public String coinShop(){
+        log.info("coinShop");
+        return "/coinShop";
     }
 }
