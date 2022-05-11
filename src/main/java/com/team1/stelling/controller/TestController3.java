3a407d82bc79599f1b46611ad50fd65cdc37edcb
yewon9609 <dodnjs1241@naver.com>
2022-05-11 22:07:41 +0900

    소설상세보기페이지

--- src/main/java/com/team1/stelling/controller/TestController3.java
+++ src/main/java/com/team1/stelling/controller/TestController3.java
@@ -7,11 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j
-@RequestMapping("/main/*")
+@RequestMapping("/novel/*")
 public class TestController3 {
 
 
-    @GetMapping("/main")
+    @GetMapping("/novelDetailView")
     public void myBook(){}
 
 
