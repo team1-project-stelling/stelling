fd3532a1afa56a57ef6c4a69aa965023fcaa339f
NormalHero <mystparkkorea@gmail.com>
2022-05-13 04:05:40 +0900

    JPA - service , 단위테스트 완료

--- src/main/java/com/team1/stelling/domain/vo/SubNovelVO.java
+++ src/main/java/com/team1/stelling/domain/vo/SubNovelVO.java
@@ -4,8 +4,12 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.hibernate.annotations.Generated;
+import org.hibernate.annotations.GenerationTime;
 
 import javax.persistence.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 
@@ -18,7 +22,7 @@ import java.util.Date;
 public class SubNovelVO {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SUBNOVEL_SEQ")
-    @Column(name = "SUBNOVEL_NUMBER ")
+    @Column(name = "SUBNOVEL_NUMBER")
     Long subNovelNumber; // 회차 번호
 
 
@@ -34,26 +38,45 @@ public class SubNovelVO {
     private  String subNovelTitle; //   회차제목
     @Column(name = "SUBNOVEL_WRITERCOMMENT")
     private String subNovelWriterComment; /*작가의 작품 후기(매 회차 마다)*/
-    @Column(name = "SUBNOVEL_UPLOADDATE ")
+    @Generated(GenerationTime.INSERT)
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "SUBNOVEL_UPLOADDATE",updatable = false)
     private Date subNovelUploadDate;  // 회차 업로드 시간
-    @Column(name = "SUBNOVEL_UPDATEDATE ")
+    @Generated(GenerationTime.INSERT)
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "SUBNOVEL_UPDATEDATE")
     private  Date subNovelUpdateDate;  // 회차 수정 시간
-    @Column(name = "SUBNOVEL_VIEWCOUNT ")
+    @Column(name = "SUBNOVEL_VIEWCOUNT")
     private  int subNovelViewCount; // 조회수
-    @Column(name = "SUBNOVEL_LIKECOUNT ")
+    @Column(name = "SUBNOVEL_LIKECOUNT")
     private int subNovelLickCount;  // 회차 좋아요
-    @Column(name = "SUBNOVEL_STATUS ")
+    @Column(name = "SUBNOVEL_STATUS")
     private  int subNovelStatus;/*회차 상태 0 숨김, 1 보여짐*/
 
+    public void updateSubNovelTitle(String subNovelTitle) { this.subNovelTitle = subNovelTitle;}
+
+    public void updateSubNovelWriterComment(String subNovelWriterComment) { this.subNovelWriterComment = subNovelWriterComment; }
+
+    public void updateSubNovelUpdateDate() { this.subNovelUpdateDate = new Date(); }
+
+    public void updateSubNovelViewCount() { this.subNovelViewCount++; }
+
+    public void updateSubNovelLickCount() { this.subNovelLickCount++; }
+
+    public void updateSubNovelStatus(int subNovelStatus) { this.subNovelStatus = subNovelStatus; }
+
     @Builder
-    public SubNovelVO(Long subNovelNumber, NovelVO novelVO, UserVO userVO, String subNovelTitle, String subNovelWriterComment, Date subNovelUploadDate, Date subNovelUpdateDate, int subNovelViewCount, int subNovelLickCount, int subNovelStatus) {
+    public SubNovelVO(Long subNovelNumber, NovelVO novelVO, UserVO userVO, String subNovelTitle, String subNovelWriterComment, String subNovelUploadDate, String subNovelUpdateDate, int subNovelViewCount, int subNovelLickCount, int subNovelStatus) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         this.subNovelNumber = subNovelNumber;
         this.novelVO = novelVO;
         this.userVO = userVO;
         this.subNovelTitle = subNovelTitle;
         this.subNovelWriterComment = subNovelWriterComment;
-        this.subNovelUploadDate = subNovelUploadDate;
-        this.subNovelUpdateDate = subNovelUpdateDate;
+        try {
+            if(subNovelUploadDate != null ) { this.subNovelUploadDate = sdf.parse(subNovelUploadDate); }
+            if(subNovelUpdateDate != null ) { this.subNovelUpdateDate = sdf.parse(subNovelUpdateDate); }
+        } catch (ParseException e) { e.printStackTrace();}
         this.subNovelViewCount = subNovelViewCount;
         this.subNovelLickCount = subNovelLickCount;
         this.subNovelStatus = subNovelStatus;
--- src/main/java/com/team1/stelling/service/RecentViewService.java
+++ src/main/java/com/team1/stelling/service/RecentViewService.java
@@ -1,11 +1,21 @@
 package com.team1.stelling.service;
 
+import com.team1.stelling.domain.repository.RecentViewRepository;
+import com.team1.stelling.domain.vo.RecentViewVO;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-@Repository
 @Slf4j
+@RequiredArgsConstructor
 public class RecentViewService {
+    private  final  RecentViewRepository recentViewRepository;
+
+    public void register(RecentViewVO vo){ recentViewRepository.save(vo);}
+    public RecentViewVO get(Long rvNo){return recentViewRepository.findById(rvNo).get();}
+    public List<RecentViewVO> getList(){return recentViewRepository.findAll();}
 }
