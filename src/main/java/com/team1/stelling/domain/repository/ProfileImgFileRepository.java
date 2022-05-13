a8df1f2be8a13bd8acdae3da809fb9625017f765
NormalHero <mystparkkorea@gmail.com>
2022-05-13 18:15:00 +0900

    UUID 반영 수정

--- src/main/java/com/team1/stelling/domain/repository/ProfileImgFileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.team1.stelling.domain.repository;
-
-import com.team1.stelling.domain.vo.ProfileImgFileVO;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfileImgFileRepository extends JpaRepository<ProfileImgFileVO, Long> {
-}
