88764bb4acd8abb96b0e7729d1ffac6f20eb9d9c
NormalHero <mystparkkorea@gmail.com>
2022-05-13 13:37:23 +0900

    logstatus Scheduling 작성

---
+++ src/main/java/com/team1/stelling/aspect/LogAspect.java
@@ -0,0 +1,44 @@
+package com.team1.stelling.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+@Configuration
+@Aspect
+@Slf4j
+public class LogAspect {
+    @Before("@annotation(com.team1.stelling.aspect.annotation.LogStatus)")
+    public void beforeStart(JoinPoint joinPoint){
+        log.info("========================================================");
+        log.info("Method " + joinPoint.getSignature().getName() + " Started at " + LocalDateTime.now());
+        log.info("Arguments : " + Arrays.stream(joinPoint.getArgs()).map(String::valueOf).collect(Collectors.joining(", ")));
+        log.info("========================================================");
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
