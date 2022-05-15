816953176f40ec1b0e3948730f45413c43710612
NormalHero <mystparkkorea@gmail.com>
2022-05-16 03:56:33 +0900

    NovelCategoryDTO 추가, 조회수&좋아요 단위 반영, 엔티티 사용 -> DTO사용 전환 , 해쉬태그

--- src/main/java/com/team1/stelling/service/NovelService.java
+++ src/main/java/com/team1/stelling/service/NovelService.java
@@ -1,22 +1,29 @@
-package com.team1.stelling.domain.dto;
+package com.team1.stelling.service;
 
+import com.team1.stelling.domain.dto.NovelCategoryDTO;
 import com.team1.stelling.domain.repository.NovelRepository;
+import com.team1.stelling.domain.vo.MyPickVO;
 import com.team1.stelling.domain.vo.NovelVO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.Converter;
+import org.modelmapper.ModelMapper;
+import org.springframework.cglib.core.internal.Function;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class NovelService {
     private final NovelRepository novelRepository;
+//    private final NovelSearchRepository novelSearchRepository;
+    private final ModelMapper modelMapper;
 
     public NovelVO get(Long nNO){ return novelRepository.findById(nNO).orElseGet(null);}
     public List<NovelVO> getList(){return novelRepository.findAll();}
@@ -27,14 +34,18 @@ public class NovelService {
     }
     public int getTotal(){return novelRepository.findByIdTotal();}
 
+
+
     /* search */
     @Transactional
-    public Page<NovelVO> search(String keyword, Pageable pageable) {
-        Page<NovelVO> novelList = novelRepository.findByNovelHashtagContaining(keyword, pageable);
-        return novelList;
+    public Page<NovelCategoryDTO> search(String keyword, Pageable pageable) {
+        return  novelRepository.findByNovelHashtagContaining(keyword,pageable).map(objectEntity -> modelMapper.map(objectEntity, NovelCategoryDTO.class));
     }
     /* 페이징처리 전체조회*/
     @Transactional
-    public Page<NovelVO> getList(Pageable pageable){return novelRepository.findAll(pageable);}
+    public Page<NovelCategoryDTO> getList(Pageable pageable){
+//        novelRepository.findAll(pageable).map(objectEntity -> modelMapper.map(objectEntity, NovelCategoryDTO.class));
+        return  novelRepository.findAll(pageable).map(objectEntity -> modelMapper.map(objectEntity, NovelCategoryDTO.class));
+    }
 
 }
