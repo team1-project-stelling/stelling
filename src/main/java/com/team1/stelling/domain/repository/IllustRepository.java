75dd387b8a8c6dc759fdc7d07d4051f3cb5ddccf
docong1002 <96901629+docong1002@users.noreply.github.com>
2022-05-18 07:43:40 +0900

    일러스트 수정

--- src/main/java/com/team1/stelling/domain/repository/IllustRepository.java
+++ src/main/java/com/team1/stelling/domain/repository/IllustRepository.java
@@ -2,7 +2,10 @@ package com.team1.stelling.domain.repository;
 
 import com.team1.stelling.domain.vo.IllustVO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface IllustRepository extends JpaRepository<IllustVO, Long> {
+    @Query(value = "SELECT COUNT(USER_NUMBER) FROM TBL_ILLUST", nativeQuery = true)
+    int findByUserNumberTotal(Long userNumber);
 
 }
