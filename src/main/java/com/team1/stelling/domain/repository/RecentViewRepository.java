c57bffa41bbacaeb7dbf2673288a506f1f442013
NormalHero <mystparkkorea@gmail.com>
2022-05-18 22:34:19 +0900

    중간 보고용 커밋 - 랭킹 기타수정, 내 서재 레이아웃 수정 및 기능 구현

--- src/main/java/com/team1/stelling/domain/repository/RecentViewRepository.java
+++ src/main/java/com/team1/stelling/domain/repository/RecentViewRepository.java
@@ -1,8 +1,10 @@
 package com.team1.stelling.domain.repository;
 
 import com.team1.stelling.domain.vo.RecentViewVO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RecentViewRepository extends JpaRepository<RecentViewVO, Long> {
-
+    Page<RecentViewVO> findByUserVO_UserNumber(Long userNumber, Pageable pageable);
 }
