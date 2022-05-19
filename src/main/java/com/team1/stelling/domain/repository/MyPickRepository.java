29d52a7900f10ac1733d073acbb9a57b93419406
NormalHero <mystparkkorea@gmail.com>
2022-05-19 15:27:10 +0900

    찜 삭제 구현, 찜 한 소설의 up 유무 라벨 추가

--- src/main/java/com/team1/stelling/domain/repository/MyPickRepository.java
+++ src/main/java/com/team1/stelling/domain/repository/MyPickRepository.java
@@ -5,9 +5,12 @@ import com.team1.stelling.domain.vo.NovelVO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface MyPickRepository extends JpaRepository<MyPickVO, Long> {
     Page<MyPickVO> findByUserVO_UserNumberAndNovelVO_NovelHashtagContaining(Long userNumber,String keyword, Pageable pageable);
     Page<MyPickVO> findByUserVO_UserNumber(Long userNumber,Pageable pageable);
+    @Transactional
+    int deleteByMyPickNumber(Long myPickNumber);
 
 }
