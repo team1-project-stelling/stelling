63abd59ed3849d59c088eec0b5d890588e39d64d
Ryeol93 <cksduf_93@naver.com>
2022-05-24 10:38:41 +0900

    커밋

--- src/main/java/com/team1/stelling/domain/repository/InquiryRepository.java
+++ src/main/java/com/team1/stelling/domain/repository/InquiryRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface InquiryRepository extends JpaRepository<InquiryVO, Long> {
     Page<InquiryVO> findByUserVO_UserNumber(Long userNum, Pageable pageable);
 
-public interface InquiryRepository extends JpaRepository<InquiryVO, Long> {
-    InquiryVO findByUserVO_UserNumber(Long userNumber);
-
+//public interface InquiryRepository extends JpaRepository<InquiryVO, Long> {
+//    InquiryVO findByUserVO_UserNumber(Long userNumber);
 }
