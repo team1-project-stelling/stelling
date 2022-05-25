b9569672aaf69a5ea5bd9e9f12c7b955f22edcec
NormalHero <mystparkkorea@gmail.com>
2022-05-26 00:19:17 +0900

    헤더 메뉴 이동 요청 경로 설정 완료 및 로그인 분기처리 완료 / 일러스트 헤더 까지 완료
    남은 헤더 작업
    
    채팅 로그아웃 요청 필요
    
    일러스트 헤더
    채팅 요청 필요
    
    * 두 요청 프로파일값 에따른 분기처리 필요
    일러스트 등록 ->
    프로파일 있어야 일러스트 등록 가능
    /illust/register
    
    프로파일 있어야 그림작업물 볼수있음
    /illust/illustUserPage

--- src/main/resources/static/assets/js/fix/header.js
+++ src/main/resources/static/assets/js/fix/header.js
@@ -1,26 +1,36 @@
 let hasTahCk = false;
-function hasTag() {
+function hashTag() {
     let $form = $(nav_searchForm);
     if(!hasTahCk) {
 
 
         $('#nav_searchForm input ').css( 'textIndent', '26px');
-        $('#search_all button').css('transform', 'translateX(-262px)')
+        $('#search_all button.tagBtn').css('transform', 'translateX(-262px)')
         // 태그로검색하기위한 조건 변경
-        $form.find("input[name='type']").val('G');
+        $form.find("input[name='type']").val('tag');
         hasTahCk = true;
     }else if(hasTahCk){
         $('#nav_searchForm input ').css( 'textIndent', '20px');
-        $('#search_all button').css('transform', 'translateX(0)')
+        $('#search_all button.tagBtn').css('transform', 'translateX(0)')
         // 전체검색을 위한 조건 변경
-        $form.find("input[name='type']").val('CTWG');
+        $form.find("input[name='type']").val('all');
         hasTahCk = false;
     }
 
 }
-$('.nav_searchForm_btn').on('click',function () {
-    alert( $('#search_all').find("input[name='keyword']").val())
+
+$("form#nav_searchForm").submit(function () {
+    if(!$("input[name='keyword']").val()){
+        alert("검색어를 입력하세요!")
+        return false;
+    }
+    return true;
 })
+
+
+/*$('.nav_searchForm_btn').on('click',function () {
+    alert( $('#search_all').find("input[name='keyword']").val())
+})*/
 $('.eventMenu').on('click',function () {
     $('div.menuArea').slideToggle(450);
 })
