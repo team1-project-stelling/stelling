211e15b134384aa41d19889251095ff01c820c9a
LeeJiHun14 <alphamalelee@naver.com>
2022-05-27 03:40:54 +0900

    이지훈 로그인 처리 // 회원가입 유효성 검사 수정

--- src/main/java/com/team1/stelling/controller/OauthController.java
+++ src/main/java/com/team1/stelling/controller/OauthController.java
@@ -1,7 +1,11 @@
 package com.team1.stelling.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.team1.stelling.domain.vo.KakaoProfile;
+import com.team1.stelling.domain.vo.UserVO;
 import com.team1.stelling.service.OauthService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -11,16 +15,21 @@ import java.util.HashMap;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/user")
+@Slf4j
 public class OauthController {
 
     private final OauthService oauthService;
 
     @RequestMapping(value="/oauth")
-    public ModelAndView login(@RequestParam("code") String code, HttpSession session) {
+    public ModelAndView login(@RequestParam("code") String code, HttpSession session, UserVO userVO) {
         ModelAndView mav = new ModelAndView();
         String accessToken = oauthService.getKakaoAccessToken(code);
         HashMap<String, Object> userInfo = oauthService.getUserInfo(accessToken);
         System.out.println("login info : " + userInfo.toString());
+
+        KakaoProfile kakaoProfile = new KakaoProfile();
+//        System.out.println(kakaoProfile.getId());
+//        System.out.println(kakaoProfile.getKakaoAccount());
 
         if(userInfo.get("email") != null) {
             session.setAttribute("userId", userInfo.get("email"));
