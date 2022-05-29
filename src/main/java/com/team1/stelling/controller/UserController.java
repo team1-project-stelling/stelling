270b8eeaeee0e017b0b474178c1fa435359fae32
LeeJiHun14 <alphamalelee@naver.com>
2022-05-30 03:03:06 +0900

    이지훈 - 코드 정리

--- src/main/java/com/team1/stelling/controller/UserController.java
+++ src/main/java/com/team1/stelling/controller/UserController.java
@@ -98,6 +98,7 @@ public class UserController {
     public @ResponseBody String sendSMS (String phoneNumber) {
         Random rand  = new Random();
         String numStr = "";
+
         for(int i=0; i<4; i++) {
             String ran = Integer.toString(rand.nextInt(10));
             numStr += ran;
